cee1558df13be7e86b80ff9d9d0fc0861eab738c
anhtuta <anhtu51195@gmail.com>
2025-05-17 18:33:46 +0700

    Clone pagination from nenmongvn

---
+++ components/Pagination.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Link from "next/link";
+import { pagination, active } from "./Pagination.module.scss";
+
+/**
+ * Cách làm: Nếu totalPages < 10 thì hiển thị toàn bộ các số từ 1-9, nếu ko, hiển thị 3 phần:
+ * - Phần 1: hiển thị currentPage và 2 số cạnh nó (có thể là 2 số bên trái, 2 số bên phải hoặc 1 trái 1 phải)
+ * - Phần 2,3: mỗi phần hiển thị 2 số và dấu ...
+ */
+export default function Pagination(props) {
+  const { baseURL, totalPages, currentPage } = props;
+  const pageArray = [];
+  if (totalPages < 10) {
+    [...Array(totalPages)].map((_, i) => {
+      pageArray.push(i + 1);
+      return null;
+    });
+  } else {
+    if (currentPage <= 4) {
+      pageArray.push(1, 2, 3, 4, 5, "...", totalPages - 2, totalPages - 1, totalPages);
+    } else if (currentPage >= totalPages - 3) {
+      pageArray.push(1, 2, 3, "...", totalPages - 4, totalPages - 3, totalPages - 2, totalPages - 1, totalPages);
+    } else {
+      pageArray.push(1, 2, "...", currentPage - 1, currentPage, currentPage + 1, "...", totalPages - 1, totalPages);
+    }
+  }
+  if (totalPages <= 1) return "";
+  return (
+    <div className={pagination}>
+      <Link href={currentPage <= 2 ? baseURL : `${baseURL}/${currentPage - 1}`}>
+        <i className="fa fa-angle-double-left" aria-hidden="true"></i>
+      </Link>
+      {pageArray.map((page, index) => {
+        if (page !== currentPage && page !== "...")
+          return (
+            <Link href={page === 1 ? baseURL : `${baseURL}/${page}`} key={index}>
+              {page}
+            </Link>
+          );
+        else
+          return (
+            <span className={page !== currentPage ? "" : active} key={index}>
+              {page}
+            </span>
+          );
+      })}
+      <Link href={currentPage === totalPages ? `${baseURL}/${currentPage}` : `${baseURL}/${currentPage + 1}`}>
+        <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+      </Link>
+    </div>
+  );
+}
