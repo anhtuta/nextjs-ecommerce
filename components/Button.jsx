dcf7e525808c44fb9e945c1b5b1881ca1db534f7
anhtuta <anhtu51195@gmail.com>
2025-05-24 23:53:03 +0700

    Button: add custom class, no need to set height

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { btn } from './Button.module.scss';
+import React from "react";
+import { btn } from "./Button.module.scss";
 
-export default function Button(props) {
-  const { text, onClick, disabled = false } = props;
+export default function Button({ text, className = "", onClick, disabled = false }) {
   function handleOnClick(e) {
     e.preventDefault();
     onClick();
   }
 
   return (
-    <button
-      type="submit"
-      className={btn}
-      onClick={handleOnClick}
-      disabled={disabled}
-    >
+    <button type="submit" className={btn + " " + className} onClick={handleOnClick} disabled={disabled}>
       {text}
     </button>
   );
